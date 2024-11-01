a724cc0daf22fa3412766bab1ff3f3adc80c0dfe
Michael Padiernos <mike@padiernos.me>
2024-11-01 00:37:00 -0400

    [theme] updated statusline colors

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -9,10 +9,10 @@ User_accent   = "#efe100"
 User_fg       = "#ffffff"
 User__fg      = "#ffffff"
 User_bg       = "#000000"
-User__bg      = "#000000"
+User__bg      = "#171717"
 
 M.base46 = {
-	theme = "gatekeeper",
+  theme = "gatekeeper",
 
   hl_override = {
     Normal = { bg = User_bg },
@@ -21,9 +21,19 @@ M.base46 = {
     NvimTreeNormal  = { bg = User_bg },
     NvimTreeNormalNC = { bg = User_bg },
     TabLineSel = { bg = User_bg },
-    St_EmptySpace  = { bg = User_bg },
-    St_gitIcons = { bg = User_bg },
+    StatusLine = { bg = User_bg },
+    St_NormalMode = { bg = User_accent },
+    St_NormalModeSep = { fg = User_accent, bg = User_accent },
+    -- St_file = { bg = User__bg },
+    -- St_file_sep = { bg = User__bg },
+    -- St_cwd_sep = { bg = User__bg },
+    -- St_pos_sep = { bg = User__bg },
+    St_Lsp = { bg = "#0d0f19" },
     St_LspMsg = { bg = User_bg },
+    St_EmptySpace  = { bg = User_bg },
+    St_cwd_text = { fg = "#fe1a66", bg = "#1e030c" },
+    St_gitIcons = { bg = User_bg },
+    St_pos_text = { bg = "#001708"},
   }
 }
 
